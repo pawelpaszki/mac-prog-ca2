6aa1be7dabbabfc399664f1d54211006f82e4659
Pawel Paszki <pawelpaszki@Pawels-Mac-mini.local>
2018-04-07 16:43:06 +0100

    added warning labels for non-populated textfields in add exercise view

--- GymGuide/AddExerciseViewController.swift
+++ GymGuide/AddExerciseViewController.swift
@@ -11,11 +11,25 @@ import UIKit
 class AddExerciseViewController: UIViewController {
     
     var muscleName: String!
+    @IBOutlet weak var exerciseNameRequired: UILabel!
+    @IBOutlet weak var videoIdRequired: UILabel!
+    @IBOutlet weak var imageUrlRequired: UILabel!
+    @IBOutlet weak var descriptionRequired: UILabel!
+    @IBOutlet weak var createExerciseButton: UIButton!
+    @IBOutlet weak var exerciseNameTextField: UITextField!
+    @IBOutlet weak var videoIdTextField: UITextField!
+    @IBOutlet weak var imageUrlTextField: UITextField!
+    @IBOutlet var descriptionTextFieldCollection: [UITextField]!
+    @IBOutlet weak var muscleNameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(muscleName)
+        
         createExerciseButton.backgroundColor = UIColor.black
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        muscleNameTextField.text = muscleName
     }
     
     @IBAction func backPressed(_ sender: UIButton) {
@@ -25,5 +39,60 @@ class AddExerciseViewController: UIViewController {
             self.dismiss(animated: false, completion: nil)
         }
     }
-    @IBOutlet weak var createExerciseButton: UIButton!
+    
+    @IBAction func exerciseNameValueChanged(_ sender: UITextField) {
+        if sender.text == "" {
+            exerciseNameRequired.isHidden = false
+        } else {
+            exerciseNameRequired.isHidden = true
+        }
+    }
+    
+    @IBAction func videoIdValueChanged(_ sender: UITextField) {
+        if sender.text == "" {
+            videoIdRequired.isHidden = false
+        } else {
+            videoIdRequired.isHidden = true
+        }
+    }
+    
+    @IBAction func imageUrlValueChanged(_ sender: UITextField) {
+        if sender.text == "" {
+            imageUrlRequired.isHidden = false
+        } else {
+            imageUrlRequired.isHidden = true
+        }
+    }
+    
+    @IBAction func descriptionValueChanged(_ sender: UITextField) {
+        if sender.text == "" {
+            descriptionRequired.isHidden = false
+        } else {
+            descriptionRequired.isHidden = true
+        }
+    }
+    
+    @IBAction func createExercisePressed(_ sender: UIButton) {
+        if allRequiredFieldsNotEmpty() {
+            
+        } else {
+            if exerciseNameTextField.text!.count == 0 {
+                exerciseNameRequired.isHidden = false
+            }
+            if videoIdTextField.text!.count == 0 {
+                videoIdRequired.isHidden = false
+            }
+            if imageUrlTextField.text!.count == 0 {
+                imageUrlRequired.isHidden = false
+            }
+            if descriptionTextFieldCollection[0].text!.count == 0 {
+                descriptionRequired.isHidden = false
+            }
+        }
+    }
+    
+    func allRequiredFieldsNotEmpty() -> Bool {
+        return exerciseNameTextField.text!.count > 0 && videoIdTextField.text!.count > 0
+            && imageUrlTextField.text!.count > 0 && descriptionTextFieldCollection[0].text!.count > 0
+    }
 }
